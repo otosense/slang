3372e4cfdeb6b2f8f5612a54e7b9d6784c7f0f65
twhalen <thor@otosense.com>
2020-04-16 15:29:15 -0400

    v 0.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='slang',
-    version='0.0.1',
+    version='0.0.2',
     description='Tools to endow signals with language-like structure.',
     long_description=readme(),
     long_description_content_type="text/markdown",
