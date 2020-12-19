56c2e42466e6e7d9697000b09ffbd3b5a1fde72b
Thor Whalen <thorwhalen1@gmail.com>
2020-12-18 17:58:22 -0800

    0.0.7

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-def text_of_readme_md_file():
-    try:
-        with open('README.md') as f:
-            return f.read()
-    except:
-        return ""
-
-
-setup(
-    packages=find_packages(),
-    long_description=text_of_readme_md_file(),
-    long_description_content_type="text/markdown"
-)  # Note: Everything should be in the local setup.cfg
+setup()  # Note: Everything should be in the local setup.cfg
